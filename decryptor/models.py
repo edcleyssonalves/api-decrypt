7b310baea0dd619f44e63f81b3a756ae8baad942
Your Name <you@example.com>
2025-08-11 12:18:37 -0300

    update decryptor/models.py

--- decryptor/models.py
+++ decryptor/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class PaymentProof(models.Model):
     # dados do contato
     contact_name = models.CharField(max_length=255, blank=True, null=True)
-    contact_number = models.CharField(max_length=15)
+    contact_number = models.CharField(max_length=50)
     chat_id = models.CharField(max_length=255, blank=True, null=True)
 
     # dados da midia
